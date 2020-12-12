662db18ffa039dab9810c34e03b44947e2e9ef14
AbdLaswi <abed.laswii@gmail.com>
2020-12-12 23:37:57 +0200

    change file names

--- back-end/Models/User/getUser.js
+++
@@ -1,9 +0,0 @@
-const getUserByName = (req, res) => {
-  const userId = req.params.username;
-  const query = `SELECT * FROM user WHERE username = ?`;
-  connection.query(query, userId, (err, result) => {
-    if (err) throw err;
-    res.json(result);
-  });
-};
-module.exports = getUserByName;
