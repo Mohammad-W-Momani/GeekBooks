f7b6651159fcb335a2fe017443e45ec16c11fa0c
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-12-17 07:09:52 +0200

    Finish login and password page

--- front-end/src/component/Entry/Password/Password.js
+++ front-end/src/component/Entry/Password/Password.js
@@ -1,7 +1,29 @@
 import React from "react";
-import "./Password.css"
+import Axios from "axios";
+import "./Password.css";
 
 function Password() {
+  const token = localStorage.getItem("token");
+
+  const [newPassword, setNewPassword] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+
+  const editPassword = () => {
+    Axios.put(
+      `profile/editpassword`,
+      { newPassword: newPassword, oldPassword: oldPassword },
+      {
+        headers: { Authorization: `Basic ${token}` },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="containers">
       <div
@@ -17,13 +39,16 @@ function Password() {
               <div className="input-group form-group">
                 <div className="input-group-prepend">
                   <span className="input-group-text">
-                  <i className="fas fa-key"></i>
+                    <i className="fas fa-key"></i>
                   </span>
                 </div>
                 <input
                   type="password"
                   className="form-control"
                   placeholder="Old password"
+                  onChange={(event) => {
+                    setOldPassword(event.target.value);
+                  }}
                 />
               </div>
               <div className="input-group form-group">
@@ -36,6 +61,9 @@ function Password() {
                   type="password"
                   className="form-control"
                   placeholder="New password"
+                  onChange={(event) => {
+                    setNewPassword(event.target.value);
+                  }}
                 />
               </div>
               <div className="form-group">
@@ -44,13 +72,14 @@ function Password() {
                   value="Save"
                   className="btn float-right password_btn"
                   style={{ background: " rgba(44, 83, 255, 0.315)" }}
+                  onClick={editPassword}
                 />
               </div>
             </form>
           </div>
           <div className="card-footer">
             <div className="d-flex justify-content-center links">
-            Already Geek?<a href="login">Login</a>
+              Already Geek?<a href="login">Login</a>
             </div>
           </div>
         </div>
