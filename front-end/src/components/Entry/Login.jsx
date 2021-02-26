139a6b67c1cf9caeb8f971b677b2ed1c76d95718
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-02-27 01:31:33 +0200

    Improve the UX

--- front-end/src/components/Entry/Login.jsx
+++ front-end/src/components/Entry/Login.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Register from "./Register";
+import Unavailable from "./EntryStyle/Img/Eye_Show_Unavailable.png";
+import View from "./EntryStyle/Img/Eye_Show_View.png";
 import "./EntryStyle/Entry.scss";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const [signInOrUp, setSignInUp] = useState(false);
-  const [onFocus, setOnFocus] = useState(true);
-  const transitions = () => {
-    setSignInUp(!signInOrUp);
-  };
+  const [onFocusName, setOnFocusName] = useState(false);
+  const [onFocusPass, setOnFocusPass] = useState(false);
 
   const [username, setUsername] = useState({});
   const [password, setPassword] = useState({});
+
+  const transitions = () => {
+    setSignInUp(!signInOrUp);
+  };
 
   const signIn = (e) => {
     e.preventDefault();
@@ -36,40 +42,56 @@ const Login = () => {
   useEffect(() => {}, []);
   return (
     <div className={`container ${signInOrUp ? "right-panel-active" : ""}`}>
-      <Register />
+      <Register Unavailable={Unavailable} View={View}/>
       <div className="container__form container--sign-in">
         <form>
           <h1>Sign in</h1>
           <br />
           <span>Use your account</span>
-          <div className={onFocus ? "username-label" : "username-label1"}>
-            {console.log(onFocus)}
+          <div className={onFocusName ? "labelShow" : "labelHiding"}>
             <label htmlFor="Email">Email</label>
           </div>
           <input
             type="text"
-            placeholder="Email"
+            placeholder="Username, Email, Phone"
             onChange={(e) => {
               setUsername(e.target.value);
             }}
             onFocus={() => {
-              setOnFocus(false);
+              setOnFocusName(true);
+            }}
+            onBlur={() => {
+              setOnFocusName(false);
             }}
             className="username-input"
-            // style={onFocus? }
           />
-          <div>
+          <div className={onFocusPass ? "labelShow" : "labelHiding"}>
             <label htmlFor="password">Password</label>
           </div>
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
+          <div id="input_container">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              id="input"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              onFocus={() => {
+                setOnFocusPass(true);
+              }}
+              onBlur={() => {
+                setOnFocusPass(false);
+              }}
+            />
+            <img
+              src={showPassword?Unavailable:View}
+              id="input_img"
+              onClick={(e) => {setShowPassword(!showPassword)}}
+            />
+          </div>
+
           <br />
-          <button className="btn--entry" onClick={signIn}>
+          <button className="btn--primary" onClick={signIn}>
             Sign In
           </button>
         </form>
@@ -79,18 +101,18 @@ const Login = () => {
       <div className="overlay-container">
         <div className="overlay">
           <div className="overlay--panel overlay--left">
-            <h1>Welcome Back!</h1>
+            <h1>Already Geeks!</h1>
             <p>
               To keep connected with us please login with your personal info
             </p>
-            <button className="btn--entry btn--ghost" onClick={transitions}>
+            <button className="btn--primary btn--ghost" onClick={transitions}>
               Sign In
             </button>
           </div>
           <div className="overlay--panel overlay--right">
             <h1>Hello, Geek!</h1>
             <p>Enter your personal details and start journey with us</p>
-            <button className="btn--entry btn--ghost" onClick={transitions}>
+            <button className="btn--primary btn--ghost" onClick={transitions}>
               Sign Up
             </button>
           </div>
--- front-end/src/components/Entry/Register.jsx
+++ front-end/src/components/Entry/Register.jsx
@@ -1,15 +1,23 @@
-import React, { useState, useRef} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-const Register = () => {
+const Register = ({ Unavailable, View }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfPassword, setShowConfPassword] = useState(false);
+
+  const [onFocusName, setOnFocusName] = useState(false);
+  const [onFocusEmail, setOnFocusEmail] = useState(false);
+  const [onFocusPhone, setOnFocusPhone] = useState(false);
+  const [onFocusPass, setOnFocusPass] = useState(false);
+  const [onFocusConfPass, setOnFocusConfPass] = useState(false);
+
   const [username, setUsername] = useState({});
   const [email, setEmail] = useState({});
   const [phone, setPhone] = useState({});
   const [password, setPassword] = useState({});
   const [confirmPassword, setConfirmPassword] = useState({});
-  
 
-
-  const signUp = () => {
+  const signUp = (e) => {
+    e.preventDefault();
     axios
       .post("/register", {
         username,
@@ -30,58 +38,113 @@ const Register = () => {
       <form>
         <h1>Create Account</h1>
         <br />
-        <div>
-        <label  htmlFor="username">Username</label>
+        <div className={onFocusName ? "labelShow" : "labelHiding"}>
+          <label htmlFor="username">Username</label>
         </div>
-          <input
-            type="text"
-            placeholder="Username"
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-            autoFocus
-          />
-        <div>
-        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          onFocus={() => {
+            setOnFocusName(true);
+          }}
+          onBlur={() => {
+            setOnFocusName(false);
+          }}
+        />
+        <div className={onFocusEmail ? "labelShow" : "labelHiding"}>
+          <label htmlFor="email">Email</label>
         </div>
-          <input
-            type="email"
-            placeholder="Email"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        <div>
-        <label htmlFor="phone">Phone</label>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          onFocus={() => {
+            setOnFocusEmail(true);
+          }}
+          onBlur={() => {
+            setOnFocusEmail(false);
+          }}
+        />
+        <div className={onFocusPhone ? "labelShow" : "labelHiding"}>
+          <label htmlFor="phone">Phone</label>
         </div>
-          <input
-            type="tel"
-            placeholder="Phone"
-            onChange={(e) => {
-              setPhone(e.target.value);
-            }}
-          />
-        <div>
-        <label htmlFor="password">Password</label>
+        <input
+          type="tel"
+          placeholder="Phone"
+          onChange={(e) => {
+            setPhone(e.target.value);
+          }}
+          onFocus={() => {
+            setOnFocusPhone(true);
+          }}
+          onBlur={() => {
+            setOnFocusPhone(false);
+          }}
+        />
+        <div className={onFocusPass ? "labelShow" : "labelHiding"}>
+          <label htmlFor="password">Password</label>
         </div>
+        <div id="input_container">
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Password"
+            id="input"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
+            onFocus={() => {
+              setOnFocusPass(true);
+            }}
+            onBlur={() => {
+              setOnFocusPass(false);
+            }}
           />
-        <div>
-        <label htmlFor="confirmPassword">Confirm Password</label>
+          <img
+            src={showPassword ? Unavailable : View}
+            id="input_img"
+            onClick={() => {
+              setShowPassword(!showPassword);
+            }}
+          />
         </div>
+
+        <div className={onFocusConfPass ? "labelShow" : "labelHiding"}>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+        </div>
+        <div id="input_container">
           <input
-            type="password"
+            type={showConfPassword ? "text" : "password"}
             placeholder="Confirm Password"
+            id="input"
             onChange={(e) => {
               setConfirmPassword(e.target.value);
             }}
+            onFocus={() => {
+              setOnFocusConfPass(true);
+            }}
+            onBlur={() => {
+              setOnFocusConfPass(false);
+            }}
           />
-        <button className="btn--entry" onClick={signUp}>
+          <img
+            src={showConfPassword ? Unavailable : View}
+            id="input_img"
+            onClick={() => {
+              setShowConfPassword(!showConfPassword);
+            }}
+          />
+        </div>
+
+        <button
+          className="btn--primary"
+          onClick={signUp}
+          style={{ marginTop: "1rem" }}
+        >
           Sign Up
         </button>
       </form>
