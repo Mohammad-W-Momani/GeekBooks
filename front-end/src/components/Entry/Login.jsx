bf60df0d9378b3d7883086855103e567a96de36b
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-02-28 00:40:30 +0200

    Edit style for input

--- front-end/src/components/Entry/Login.jsx
+++ front-end/src/components/Entry/Login.jsx
@@ -107,17 +107,18 @@ const Login = () => {
       <div className="overlay-container">
         <div className="overlay">
           <div className="overlay--panel overlay--left">
-            <h1>Already Geeks!</h1>
-            <p>
-              To keep connected with us please login with your personal info
-            </p>
+            <h1>Already Geeks?</h1>
+            <p>Enter your personal details and start journey with us</p>
+
             <button className="btn--primary btn--ghost" onClick={transitions}>
               Sign In
             </button>
           </div>
           <div className="overlay--panel overlay--right">
-            <h1>Hello, Geek!</h1>
-            <p>Enter your personal details and start journey with us</p>
+            <h1>Hello, Geek</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
             <button className="btn--primary btn--ghost" onClick={transitions}>
               Sign Up
             </button>
